d86a652e857b6762245d54b6ad8bd2396bb4979a
Tino van de Kolk <tino.kolk@rocdeleijgraaf.nl>
2022-12-02 12:02:41 +0100

    Rights Seeder leeg

---
+++ database/seeders/rightsSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Right;
+use Illuminate\Database\Seeder;
+
+class rightsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+    }
+}
