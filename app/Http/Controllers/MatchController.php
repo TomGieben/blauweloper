88dd04b6cebc37064032969f4ddeb7a5171fea6d
Tino van de Kolk <tino.kolk@rocdeleijgraaf.nl>
2022-12-06 09:46:44 +0100

    Vieuws Matches

--- app/Http/Controllers/MatchController.php
+++ app/Http/Controllers/MatchController.php
@@ -3,8 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Match;
+use App\Models\User;
 
 class MatchController extends Controller
 {
-    //
+    public function index () {
+        Match::all();
+        return view('matches.index');
+    }
+
+    public function create () {
+
+        return view('matches.create');
+    }
+
+    public function store () {
+        return redirect(route('matches.index'));
+    }
+
+    public function show () {
+        return view('matches.show');
+    }
+
+    public function edit () {
+        return view('matches.edit');
+    }
+
+    public function update () {
+        return redirect(route('matches.index'));
+    }
+
+    public function delete () {
+        return redirect(route('matches.index'));
+    }
 }
