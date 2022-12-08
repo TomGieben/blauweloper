3537fd818b4e6b2e8e21732b7c008a921db5e1d0
TomGieben <t.gieben@outlook.com>
2022-12-08 13:13:22 +0100

    chess game added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -74,6 +74,10 @@
                 </div>
             </div>
         </div>
+
+        <div class="col-auto">
+            <iframe style="width: 400px; height: 560px;" src="https://www.chess.com/daily_puzzle" frameborder="0"></iframe>
+        <div>
     </div>
 </div>
 @endsection
